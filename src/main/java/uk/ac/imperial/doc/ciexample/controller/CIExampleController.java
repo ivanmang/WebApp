0116cead4418b4407e4ee582ef7fb4e79a08eed2
kfc216 <kfc216@ic.ac.uk>
2018-05-31 21:55:58 +0100

    new branch for CI deployment

--- src/main/java/uk/ac/imperial/doc/ciexample/controller/CIExampleController.java
+++ src/main/java/uk/ac/imperial/doc/ciexample/controller/CIExampleController.java
@@ -10,34 +10,28 @@ import uk.ac.imperial.doc.ciexample.CIExampleBasic;
 
 public class CIExampleController extends AbstractController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
+  @Override
+  protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                               HttpServletResponse response) throws Exception {
 
-		String paramA = request.getParameter("a");
-		String paramB = request.getParameter("b");
-		String paramAction = request.getParameter("action");
-		
-		ModelAndView model = new ModelAndView("ciexample");
-		if(paramA != null)
-			model.addObject("a", request.getParameter("a"));
-		
-		if(paramB != null)
-			model.addObject("b", request.getParameter("b"));
-		
-		if(paramAction != null) {
-			String action = request.getParameter("action");
-			if(action.equals("add") || action.equals("sub"))
-				model.addObject("action", action);
-		}
-		
-		CIExampleBasic cibasic = new CIExampleBasic();
-		if(paramA != null && paramB != null && paramAction != null) {
-			cibasic.setParams(paramA, paramB, paramAction);
-			model.addObject("result", cibasic.runAction());
-		}
-		
-		return model;
-	}
+    String paramA = request.getParameter("id");
+    String paramB = request.getParameter("name");
+
+    ModelAndView model = new ModelAndView("ciexample");
+    if(paramA != null)
+      model.addObject("id", request.getParameter("id"));
+
+    if(paramB != null)
+      model.addObject("name", request.getParameter("name"));
+
+
+    CIExampleBasic cibasic = new CIExampleBasic();
+    if(paramA != null && paramB != null) {
+      cibasic.setParams(paramA, paramB);
+      model.addObject("result");
+    }
+
+    return model;
+  }
 
 }
